dbf427611a4d8626d86d2856ebe784c7454c52ee
Shubham Kumar Jaiswal <krshubhamjaiswal2000@gmail.com>
2022-09-29 21:10:06 -0700

    NDTV website

---
+++ ndtv-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Navbar from './Components/Navbar';
+import NewsContainer from './Components/NewsContainer';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <NewsContainer />
+    </div>
+  );
+}
+
+export default App;
